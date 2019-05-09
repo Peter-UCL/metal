7906085d70c5a1c535f117aa10063dd3e7825546
Peter-UCL <49399312+Peter-UCL@users.noreply.github.com>
2019-05-09 19:18:20 +0100

    Output halves of features as inputs and sequence
    Output the first half of the input columns as input ids and the second half of columns as token type (sequence) ids

--- metal/mmtl/modules.py
+++ metal/mmtl/modules.py
@@ -16,6 +16,18 @@ class MetalModuleWrapper(nn.Module):
     def forward(self, X):
         # The object that is passed out must be different from the object that gets
         # passed in so that cached outputs from intermediate modules aren't mutated
-        X_out = {k: v for k, v in X.items()}
-        X_out["data"] = self.module(X["data"])
-        return X_out
+        if is_input:
+            half_width_of_sequence = X["data"].shape[1] / 2
+            if half_width_of_sequence % 2 != 0:
+                raise "Token input of length n must be of form n input ids and n segment ids"
+            half_width_of_sequence = int(half_width_of_sequence)
+            X_out = {k: v for k, v in X.items()}
+
+            input_ids = X["data"][:, :half_width_of_sequence]
+            token_type_ids = X["data"][:, half_width_of_sequence:]
+            X_out["data"] = self.module(input_ids=input_ids, token_type_ids=token_type_ids)
+            return X_out
+        else:
+            X_out = {k: v for k, v in X.items()}
+            X_out["data"] = self.module(X["data"])
+            return X_out
